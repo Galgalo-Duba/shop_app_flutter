0c3ba849c25b641298f770a5d59ab229d91b73b3
galgeduba <galgeduba64@gmail.com>
2025-02-28 19:15:56 +0100

    cart page

--- shop_app_flutter/lib/main.dart
+++ shop_app_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app_flutter/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,40 +10,43 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shopping App',
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromRGBO(254, 206, 1, 1),
-          primary: const Color.fromRGBO(254, 206, 1, 1),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          hintStyle: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
+    return ChangeNotifierProvider(
+      create: (context) => ChangeNotifier(),
+      child: MaterialApp(
+        title: 'Shopping App',
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromRGBO(254, 206, 1, 1),
+            primary: const Color.fromRGBO(254, 206, 1, 1),
           ),
-          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
-        ),
-        textTheme: TextTheme(
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 35,
+          inputDecorationTheme: InputDecorationTheme(
+            hintStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
           ),
-          titleMedium: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
+          textTheme: TextTheme(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 35,
+            ),
+            titleMedium: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+            bodySmall: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
           ),
-          bodySmall: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
+          appBarTheme: AppBarTheme(
+            titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
           ),
         ),
-        appBarTheme: AppBarTheme(
-          titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
-        ),
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
   }
 }
